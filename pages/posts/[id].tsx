b4cbdc4a4bd9d1089d5579db954f9cbf8d66424b
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-10 15:21:58 -0500

    Add dynamic posts page

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router";
+import connect from "../../lib/db";
+import { ObjectId } from "mongodb";
+
+export default function Post({ post }) {
+  console.log(post);
+  return (
+    <h1>Post</h1>
+  );
+}
+
+// can't use getStaticPaths since posts update frequently
+export async function getServerSideProps(context) {
+  const conn = await connect();
+  const db = conn.db();
+
+  // context params can be used to find the dynamic route
+  const post = await db.collection("posts").findOne({_id: new ObjectId(context.params.id)});
+
+  // id has to be converted to string
+  return {
+    props: {
+      post: {
+        ...post,
+        _id: post._id.toString()
+      }
+    }
+  }
+}
