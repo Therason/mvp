2d5ee6b19be0840e9dbbf9fb2bb570ccd483b927
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-09 20:15:18 -0500

    Hopefully setup credentials provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,43 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-import { CredentialsProvider } from "next-auth/providers";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
+import connect from "../../../lib/db";
+import { verify } from "../../../lib/auth";
+
+export default NextAuth({
+  session: {
+    strategy: "jwt",
+  },
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    CredentialsProvider({
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials) throw new Error("No credentials provided");
+
+        const conn = await connect();
+        const users = conn.db().collection("users");
+
+        const user = await users.findOne({
+          username: credentials?.username,
+        });
+
+        if (!user) {
+          await conn.close();
+          throw new Error("User not found");
+        }
+
+        const valid = await verify(credentials?.password, user.password);
+        if (!valid) {
+          await conn.close();
+          throw new Error("Invalid password");
+        }
+
+        await conn.close();
+        return { username: user.username, id: user.id };
+      },
     }),
   ],
-};
-
-export default NextAuth(authOptions);
+});
