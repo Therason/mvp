edd8e5b02e168d803fa4d10508bc20dcba55bd03
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-10 15:40:20 -0500

    Add individual post redirect to browse page

--- pages/browse.tsx
+++ pages/browse.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Browse({ data }) {
-  console.log(data)
+  const router = useRouter();
+
+  // redirect to post
+  const handleClick = (e: React.MouseEvent<HTMLImageElement>) => {
+    router.push(`/posts/${(e.target as HTMLElement).id}`)
+  }
+
   return (
     <>
       <h1>Browse</h1>
-      {data.map((post) => <Image key={post._id} src={post.url} alt={post.description} width="400" height="400" style={{objectFit: 'cover'}} />)}
+      {data.map((post) => <Image onClick={handleClick} id={post._id} key={post._id} src={post.url} alt={post.description} width="400" height="400" style={{objectFit: 'cover'}} />)}
     </>
   )
 }
