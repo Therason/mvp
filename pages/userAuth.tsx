7357e25116a843d47c3c4c15f26c355d1aab458d
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-10 21:17:09 -0500

    More basic styling for login and sign up forms

--- pages/userAuth.tsx
+++ pages/userAuth.tsx
@@ -6,8 +6,48 @@ import styled from "styled-components";
 const Container = styled.div`
   width: 30vw;
   min-width: 300px;
+  height: 70vh;
+  padding: 10px 20px;
   border: 1px solid gray;
+  border-radius: 5px;
   margin: 20px 50px;
+  gap: 10px;
+  display: flex;
+  flex-direction: column;
+  background: rgb(32, 30, 30);
+
+  p {
+    cursor: pointer;
+    width: fit-content;
+    padding: 5px 10px;
+  }
+
+  button {
+    margin-top: auto;
+    background: none;
+    border: none;
+    color: inherit;
+    cursor: pointer;
+    width: fit-content;
+    font-size: 1rem;
+    padding: 5px 10px;
+  }
+
+  button:hover, p:hover {
+    background: #3a3737;
+    border-radius: 5px;
+  }
+
+  input {
+    background: none;
+    border: none;
+    border-bottom: 1px solid gray;
+    color: inherit;
+  }
+
+  input:focus {
+    outline-width: 0;
+  }
 `;
 
 // component for logging in and signing up
@@ -19,7 +59,7 @@ export default function UserAuth() {
       <Container>
         <h1>Login</h1>
         <LoginForm />
-        <button onClick={() => setLogin(false)}>Don&apos;t have an account?</button>
+        <p onClick={() => setLogin(false)}>Don&apos;t have an account?</p>
       </Container>
     )
   }
@@ -28,7 +68,7 @@ export default function UserAuth() {
     <Container>
       <h1>Sign Up</h1>
       <SignupForm />
-      <button onClick={() => setLogin(true)}>Have an account already?</button>
+      <p onClick={() => setLogin(true)}>Have an account already?</p>
     </Container>
   )
 }
