923305eb4b3146b8034ddd1a705e0be9acd5b72c
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-14 11:21:04 -0500

    Add basic sign up form validation messages

--- components/signupForm.tsx
+++ components/signupForm.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 
@@ -6,20 +6,33 @@ export default function SignupForm() {
   const username = useRef<HTMLInputElement>(null);
   const password = useRef<HTMLInputElement>(null);
 
+  const [ validUsername, setValidUsername ] = useState<boolean>(true);
+  const [ validPass, setValidPass ] = useState<boolean>(true);
+  const [ notTaken, setNotTaken ] = useState<boolean>(true);
+
   const router = useRouter();
 
   // POST to API endpoint
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+
+    setValidPass(true);
+    setValidUsername(true);
+    setNotTaken(true);
+
     if (!username.current || !password.current) return;
     const enteredUser = username.current.value
     const enteredPass = password.current.value
 
-    // TODO: better form validation
-    if (enteredPass.length < 8 || enteredUser.length === 0) {
-      alert('Please enter a username and secure password');
+    // basic form validation
+    if (enteredUser.length === 0) {
+      setValidUsername(false);
       return;
-    };
+    }
+    if (enteredPass.length < 8) {
+      setValidPass(false);
+      return;
+    }
 
     try {
       const res = await fetch('/api/auth/signup', {
@@ -46,7 +59,7 @@ export default function SignupForm() {
       };
     } catch {
       // user likely already exists(?)
-      alert('Username taken!');
+      setNotTaken(false);
     }
   }
 
@@ -55,9 +68,12 @@ export default function SignupForm() {
       <label>Username:
         <input type="text" ref={username}></input>
       </label>
+      {!validUsername && <p style={{ color: '#f44250' }}>Please enter a username!</p>}
+      {!notTaken && <p style={{ color: '#f44250' }}>Username taken!</p>}
       <label>Password:
         <input type="password" ref={password}></input>
       </label>
+      {!validPass && <p style={{ color: '#f44250' }}>Password must be at least 8 characters!</p>}
       <button onClick={handleSubmit}>Submit</button>
     </>
   )
